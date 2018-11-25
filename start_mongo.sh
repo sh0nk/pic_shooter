9b9e5cdda01646bbba47ba17ef751f5951cb39ef
sh0nk <sh0nk.developer@gmail.com>
2018-11-25 17:57:07 +0900

    Handle a new and an old container on start_mongo.sh

--- start_mongo.sh
+++ start_mongo.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-# docker run -p 28001:27017 --name dev-mongo -d mongo
-docker start 77067976a678
+if [ "$(docker ps | grep dev-mongo)" != "" ]
+then
+    echo "container dev-mongo exists. starting it..."
+    docker start dev-mongo
+else
+    echo "container dev-mongo does not exists. calling docker run ..."
+    docker run -p 28001:27017 --name dev-mongo -d mongo
+fi
